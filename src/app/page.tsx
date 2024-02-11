9d872a80172575a6b3bc9489f8de385ff27792a7
Jeppe <you@example.com>
2024-02-11 22:56:05 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@
 import ProductRain from "@/components/productrain";
 import { GetAllProducts } from "@/utils/productService";
 
-
+const categories:string[] = ["electronics",
+"jewelery",
+"men's clothing",
+"women's clothing"]
 
 
 const Home = async ()  => {
@@ -10,6 +13,11 @@ const Home = async ()  => {
   const Products = await GetAllProducts();
  
   return(<>
+    <div className="flex items-center w-full justify-center gap-6">
+    {categories.map((category, index) => (
+       <span className="border p-4 text-lg" key={index}>{category}</span>
+   ))}
+    </div>
     <ProductRain products={Products}/>
   </>)
 }
