6e5e9514c28119b2768d06a6769ee0968ff45392
Jesper <Jesper.astrom@gmail.com>
2024-04-26 18:16:08 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 
 import Carousel from "@/components/carousel";
 import ProductRain from "@/components/productrain";
+import { Category } from "@/types/category";
 import { Promotion } from "@/types/promotion";
-import { GetAllProducts } from "@/utils/productService";
+import { GetAllProducts, getMainCategories } from "@/utils/productService";
 import Image from "next/image";
 import Link from "next/link";
 const promotion: Promotion[] = [
@@ -30,12 +31,13 @@ const promotion: Promotion[] = [
 
 const Home = async () => {
   const Products = await GetAllProducts();
+  const Categories:Category[] = await getMainCategories()
 
   return (
     <div className="px-4">
       
       <div className="mb-10 mt-6">
-      <span className="font-sans font-light text-2xl pl-1">Sandler kollektionen</span>
+      <span className="font-light text-2xl pl-1">Sandler kollektionen</span>
         <Carousel
           visibleSlidesCountDesktop={2}
           visibleSlidesCountTablet={2}
@@ -59,31 +61,38 @@ const Home = async () => {
         </Carousel>
       </div>
       <div className="mb-10">
-        <span className="text-xl md:text-2xl pl-1 font-sans font-light">Rekommenderade produkter</span>
+        <span className="text-2xl pl-1  font-light">Våra kategorier</span>
+        
+          <div className="relative">
         <Carousel
-          visibleSlidesCountDesktop={6}
+          visibleSlidesCountDesktop={4}
           visibleSlidesCountTablet={3}
           visibleSlidesCountMobile={2}
-          useProgressBar={true}
         >
-          {Products.map((product, index) => (
+          {Categories.map((category, index) => (
+            <div className="flex flex-col min-h-full">
             <Link
-              href={`/produkter/${product.id}`}
-              className="aspect-[9/13] bg-white min-h-full min-w-full pt-4"
+              href={`/produkter/kategori/${category.name}`}
+              className="aspect-[9/13] bg-white h-full min-w-full pt-4"
               key={index}
             >
               <Image
-                className="min-w-full object-center h-full object-contain"
+                className="min-w-full object-center h-full object-fill"
                 width={900}
                 height={1300}
-                alt={product.name}
-                src={product.images[0].imageUrl}
+                alt={category.name}
+                src={category.imageUrl}
               />
+         
             </Link>
+        <span className="mt-8 text-normal pl-1  font-semibold border-b w-fit border-black pb-1">{category.name}</span>
+        </div>
           ))}
         </Carousel>
+        </div>
+   
       </div>
-      <div>
+      <div className="pt-10">
       <span className="text-xl md:text-2xl pl-1 font-sans font-light border-b border-black pb-2 md:ml-20 ml-2">Nyheter</span>
         <ProductRain products={Products} />
       </div>
