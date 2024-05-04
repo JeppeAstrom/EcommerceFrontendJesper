09dd44cf7efad0944cd03dba56fe53467e139472
JeppeAstrom <114671697+JeppeAstrom@users.noreply.github.com>
2024-05-04 10:48:25 +0200

    Update sideMenuProductRain.tsx

--- src/components/sideMenuProductRain.tsx
+++ src/components/sideMenuProductRain.tsx
@@ -19,7 +19,7 @@ if(!childCategories){
 }
   return (
   
-    <div className="flex lg:flex-col flex-row mt-4 item-center gap-3 text-normal pl-4 items-start ">
+    <div className="flex lg:flex-col flex-row mt-4 item-center gap-3 text-normal pl-4 items-start w-fit overflow-x-auto">
       {childCategories.map((item, index) => {
         return(
         <Link className={`sm:max-lg:border sm:max-lg:border-gray-500 sm:max-lg:p-3 sm:max-lg:px-5  ${item.name === activeRoute ? ' border-b border-black' : ''}`} key={index} href={`${pathname}/${item.name}`}>{item.name}</Link>
